f1077e06b9460960ab0026c2e8e1b49897f73841
mvchp <mvchacon.p@gmail.com>
2025-06-19 19:16:10 -0400

    Subo primer ejercicio en Python

---
+++ PythonApplication1.py
@@ -0,0 +1,30 @@
+﻿edad = 10
+
+if edad < 18:
+    print("Eres menor de edad.")
+elif edad >= 18 and edad < 60:
+    print("Eres un adulto.")
+elif edad == 60:
+    print("Felices 60.")
+else:
+    print("Eres adulto mayor.")
+
+
+    # -*- coding: utf-8 -*-
+
+edad = int(input("¿Cuántos años tienes? "))
+
+if edad < 0:
+    print("Edad no válida.")
+elif edad < 12:
+    print("Eres un niño o niña.")
+elif edad < 18:
+    print("Eres un adolescente.")
+elif edad < 30:
+    print("Eres un joven adulto.")
+elif edad < 60:
+    print("Eres un adulto.")
+elif edad == 60:
+    print("¡Felices 60!")
+else:
+    print("Eres un adulto mayor.")
